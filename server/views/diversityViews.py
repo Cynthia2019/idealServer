b790be964f65c1e34e5e219a0b7d1d926118a948
Cynthia2019 <ccynthia2019@gmail.com>
2023-02-13 13:00:00 -0600

    add model load

--- server/views/diversityViews.py
+++ server/views/diversityViews.py
@@ -21,26 +21,50 @@ class call_diversity(APIView):
             # data would be sth like [1.37580596e+00 5.88081175e-01 1.37995905e+00 5.13899350e-04 6.52366007e-04 4.27444853e-01]
             #[2250985995,686328363.6,1508385126,0,0,393330312.6]
             body = json.loads(request.body)
-            data = []
+            prop = []
             for d in body: 
                 for key, value in d.items(): 
-                    data.append(value)
+                    prop.append(value)
             # data = request.GET.get('data').strip('][').split(',')
-            data = np.array(data)
-            data_raw = data
-            data = (data-np.mean(data, axis=0))/np.std(data, axis=0) if np.std(data, axis=0).all() else (data-np.mean(data, axis=0))
-            idx_left = np.arange(len(data))
-            D_feature = 3000 
-            batch_size = 20 
-            Vp = RFF(data, D_feature) 
-            idx_rel, idx_abs, _ = k_Markov_dual_DPP(Vp, idx_left, batch_size=batch_size)
+            prop = np.array(prop)
+            N = prop.shape[0]
+            M = prop.shape[1]
+            zero_col = np.all(prop[..., :]==0, axis=0) 
+            prop = prop[:, ~zero_col].reshape(N, M-2)
+            prop_raw = prop
+            prop = prop_raw
+            ## data normalization
+            n_data = len(prop_raw)
+            n_threshold = 1000
+            if n_data <= n_threshold:
+                deno = pdist(prop).mean()
+            else:
+                tmp = np.arange(n_data)
+                np.random.shuffle(tmp)
+                idx_rnd = tmp[:n_threshold]
+                deno = pdist(prop[idx_rnd]).mean()
+            prop = prop_raw / deno
+            def standardize(data):  # component-wise standardization
+                return (data-np.mean(data, axis=0))/np.std(data, axis=0) 
+            prop = standardize(prop)
+
+
+            ## property diversity
+            idx_left = np.arange(len(prop))
+            D_feature = 3000  # for N(ground set) >= 10k, approximation must be used to avoid too much overhead 
+            batch_size = 20   # for N(subset) >= 100, it's recommended to iterate with a small batch size (e.g., 200 = 20 iter x 10)
+            if n_data <= 3000: # greedy algorithm
+                C = decompose_kernel(squareform(pdist(prop)))
+                idx_abs = sample_k(C['D'], batch_size)
+            else: # approximated algorithm (based on Fourier feature)
+                Vp = RFF(prop, D_feature) 
+                _, idx_abs, _ = k_Markov_dual_DPP(Vp, idx_left, batch_size=batch_size)
 
             ## diversity of the subset (mean Euclidean distance)
-            mean_dist_standardized = np.mean(pdist(data[idx_abs])) # mean Euclidean distance of standardized property
-            mean_dist_raw = np.mean(pdist(data_raw[idx_abs]))      # mean Euclidean distance of raw property
-
-
-            return JsonResponse(np.round(mean_dist_raw, 3))
-
-    def standardize(data):  # auxiliary function 
-        return (data-np.mean(data, axis=0))/np.std(data, axis=0) 
+            mean_dist_standardized = np.mean(pdist(prop[idx_abs])) # mean Euclidean distance of standardized property
+            mean_dist_raw = np.mean(pdist(prop_raw[idx_abs]))      # mean Euclidean distance of raw property
+            print(mean_dist_raw, mean_dist_standardized)
+            return JsonResponse([{
+                'raw': np.round(mean_dist_raw, 3), 
+                'standardized': np.round(mean_dist_standardized, 3)
+            }], safe=False)
--- server/views/knnViews.py
+++ server/views/knnViews.py
@@ -9,18 +9,63 @@ from rest_framework.response import Response
 from rest_framework import status 
 import os 
 import pickle
+import io 
+from pathlib import Path
 
-
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
 import numpy as np 
+
+import boto3
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class call_model(APIView): 
     
     def get(self, request): 
         if request.method == 'GET': 
             data = request.GET.get('data').strip('][').split(',')
-            name = request.GET.get('name')
-            path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'model/model_' + name)
+            dir = os.path.dirname(os.path.realpath(__file__))
+            path = dir + '/model'
             knn = pickle.load(open(path, 'rb'))
             distances, neighbor_indices = knn.kneighbors(np.array(data, ndmin=2))
 
             return JsonResponse(neighbor_indices.tolist()[0], safe=False)
+    
+    def post(self, request): 
+        if request.method == 'POST': 
+            s3 = boto3.client('s3',
+                aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+            )
+            names = []
+            # fetch all files in s3 
+            total_df = pd.DataFrame()
+            response = s3.list_objects(
+                Bucket='ideal-dataset-1', 
+            )['Contents']
+            for content in response: 
+                names.append(content['Key'])
+
+            for name in names: 
+                response = s3.get_object(
+                    Bucket='ideal-dataset-1', 
+                    Key=name
+                )['Body']
+                df = pd.read_csv(io.BytesIO(response.read()), usecols=['C11', 'C12', 'C22', 'C16', 'C26', 'C66'])
+                total_df = pd.concat([total_df, df])
+            curr_path = os.path.realpath(os.path.dirname(__file__))
+            
+            # merge the data into one big df
+            # save the final model 
+            def save_model(data, curr_path, n_neighbors=5): 
+                n_neighbors = 5
+                knn = NearestNeighbors(n_neighbors=n_neighbors, algorithm='ball_tree').fit(data)
+                #save to bin 
+                knnModel = open(curr_path+'/model', 'wb')
+                pickle.dump(knn, knnModel)
+
+                knnModel.close()
+            save_model(total_df, curr_path, 5)
+            return JsonResponse(names, safe=False)
